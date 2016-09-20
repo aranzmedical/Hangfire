06fc5c68eb84ce3badf9312716ab2a19a999181e
Nick Wareing <nwareing@gmail.com>
2016-09-20 14:47:14 +1200

    Use only signed packages

--- src/Hangfire.SqlServer/Properties/AssemblyInfo.cs
+++ src/Hangfire.SqlServer/Properties/AssemblyInfo.cs
@@ -5,7 +5,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Hangfire.SqlServer")]
 [assembly: AssemblyDescription("SQL Server job storage for Hangfire")]
 [assembly: Guid("3d96bf2f-8854-4872-aee3-faf81d121a4d")]
-
-[assembly: InternalsVisibleTo("Hangfire.SqlServer.Tests")]
-// Allow the generation of mocks for internal types
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
